f3c0a1e99e159babc0e2a311d1e4a288c1a3962d
alissonbrg32 <alissonbrg32@gmail.com>
2024-11-29 21:37:14 +0000

    "Theres no projects yet" if no projects are found

--- src/components/UserDashboard/AllProjects/index.tsx
+++ src/components/UserDashboard/AllProjects/index.tsx
@@ -16,20 +16,13 @@ function Project({name, sharedWith, onClick}: {name: string, sharedWith: {user_i
 }
 
 export default function AllProjects({setProject} : {setProject:Function}){
-    const [projects, setProjects] = useState<ProjectInterface[]>([
-        {
-            id: "0",
-            name: "Game Development Tasks",
-            owner_id: "",
-            sharedWith: [],
-            lists: []
-        }
-    ]);
+    const [projects, setProjects] = useState<ProjectInterface[]>([]);
     return (
     <div className="flex flex-col p-4">
         <h1 className="font-bold text-xl">All projects that you are in</h1>
         <ul className="py-4">
             {projects.map(project=>{return <li><Project sharedWith={project.sharedWith} name={project.name} onClick={()=>setProject(project)}/></li>})}
+            {projects.length == 0 && <p className="text-sm opacity-80">Theres no projects yet.</p>}
         </ul>
     </div>
     )
